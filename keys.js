838a294cd04fb96a274e1f6edfc31478b13954b8
Zach Keeshin <zkeesh@gmail.com>
2017-04-07 23:37:08 -0500

    setting up Twitter API dev access

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'pLaPvHOzGOC5iGK9jtue3XY1z',
+  consumer_secret: 'oSnUFaoP3q9OCOgb8ziYYMVCqGqDjQFPcCBGLmjAnvTPd3mpPt',
+  access_token_key: '24117933-Ifi1JIoQi7idvCrNB4IaaXhosa1Kq6bsFumuEx8wn',
+  access_token_secret: 'k8Y7GKzSV4chwWPhVTcnet08NhZT9vlIYSiiGrWdSkVcC',
+}
